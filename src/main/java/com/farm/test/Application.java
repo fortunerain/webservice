166667184d2cfb38da809b627e33704fea539c95
NOHMINHO <fortunerain@naver.com>
2020-06-28 20:16:21 +0900

    네이버 로그인 및 테스트 코드 수정 완료

--- src/main/java/com/farm/test/Application.java
+++ src/main/java/com/farm/test/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing 따로 구분하기 위해 제거함.
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
--- src/test/java/com/farm/test/web/HelloControllerTest.java
+++ src/test/java/com/farm/test/web/HelloControllerTest.java
@@ -1,44 +1,54 @@
 package com.farm.test.web;
 
+import com.farm.test.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+// @Repository, @Service, @Component, @Configuration는 스캔 대상이 아님. SecurityConfig는 읽었지만 생성하기 위해 필요한 CustomOAuth2UserService 는 읽을수가 없음.
+@WebMvcTest(controllers = HelloController.class,
+  excludeFilters = {
+    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+  }
+)
 public class HelloControllerTest {
 
   @Autowired
   private MockMvc mockMvc;
 
   @Test
+  @WithMockUser(roles = "USER")
   public void hello() throws Exception {
     String hello = "hello";
     mockMvc.perform(get("/hello"))
-        .andExpect(status().isOk())
-        .andExpect(content().string(hello));
+      .andExpect(status().isOk())
+      .andExpect(content().string(hello));
   }
 
   @Test
+  @WithMockUser(roles = "USER")
   public void helloDto() throws Exception {
     String name = "test";
     int amount = 1000;
 
     mockMvc.perform(get("/hello/dto")
-    .param("name", name)
-    .param("amount", String.valueOf(amount)))
-        .andExpect(status().isOk())
-        .andExpect((ResultMatcher) jsonPath("$.name", is(name)))
-        .andExpect((ResultMatcher) jsonPath("$.amount", is(amount)));
+      .param("name", name)
+      .param("amount", String.valueOf(amount)))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.name", is(name)))
+      .andExpect(jsonPath("$.amount", is(amount)));
   }
 }
