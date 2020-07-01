37e2c937d693f26372a04fffcf8254784dcd537d
NOHMINHO <fortunerain@naver.com>
2020-07-01 11:01:18 +0900

    무중단 배포를 위한 ProfileController 추가

--- src/main/java/com/farm/test/config/auth/SecurityConfig.java
+++ src/main/java/com/farm/test/config/auth/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       .csrf().disable().headers().frameOptions().disable()  //h2-console 화면을 사용하기 위해 해당 옵션을 disable 시킴. 403 에러 발생함.
       .and()
       .authorizeRequests()  // url별 권한 관리를 설정하는 옵션. 이게 있어야 antMatchers 옵션 사용가능함.
-      .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+      .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
       .antMatchers("/api/v1/**").hasRole(Role.USER.name())
       .anyRequest().authenticated() // 위 설정된 url 제외한 나머지 request 들은 모두 로그인한 사용자만 허용함.
       .and()
---
+++ src/test/java/com/farm/test/web/ProfileControllerUnitTest.java
@@ -0,0 +1,62 @@
+package com.farm.test.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+  @Test
+  public void getRealProfile() {
+    //given
+    String expectedProfile = "real";
+    MockEnvironment env = new MockEnvironment();
+    env.addActiveProfile(expectedProfile);
+    env.addActiveProfile("oauth");
+    env.addActiveProfile("real-db");
+
+    ProfileController profileController = new ProfileController(env);
+
+    //when
+    String profile = profileController.profile();
+
+    //then
+    assertThat(profile).isEqualTo(expectedProfile);
+
+  }
+
+  @Test
+  public void getRealProfileFirst() {
+    //given
+    String expectedProfile = "oauth";
+    MockEnvironment env = new MockEnvironment();
+    env.addActiveProfile(expectedProfile);
+    env.addActiveProfile("real-db");
+
+    ProfileController profileController = new ProfileController(env);
+
+    //when
+    String profile = profileController.profile();
+
+    //then
+    assertThat(profile).isEqualTo(expectedProfile);
+
+  }
+
+  @Test
+  public void getDefaultProfile() {
+    //given
+    String expectedProfile = "default";
+    MockEnvironment env = new MockEnvironment();
+
+    ProfileController profileController = new ProfileController(env);
+
+    //when
+    String profile = profileController.profile();
+
+    //then
+    assertThat(profile).isEqualTo(expectedProfile);
+
+  }
+}
