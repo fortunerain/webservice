8f785fb5c2eee352eb1b2af5298270f5f7820d4e
NOHMINHO <fortunerain@naver.com>
2020-07-01 18:14:31 +0900

    real1 real2 yml 파일 분리, sh 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,8 @@ CURRENT_PID=$(pgrep -fl webservice | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID" ]; then
+if [ -z "$CURRENT_PID" ]
+then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
@@ -34,6 +35,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
--Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
+-Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
 -Dspring.profiles.active=real \
 $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
